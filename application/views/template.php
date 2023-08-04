64325da8618290187dcb70c92b96fd13a3b04385
ikusuba <73406832+ikusuba@users.noreply.github.com>
2023-08-04 15:00:49 +0700

    Fix error redirect

--- application/views/template.php
+++ application/views/template.php
@@ -850,7 +850,7 @@
           var audio = document.getElementById('bell-ring');
           const id_jadwal_konsultasi = JSON.parse(payload.data.body).id_jadwal_konsultasi;
           audio.play();
-          location.href = "<?php echo base_url('pasien/ResepDokter/konfirmasi?id_jadwal_konsultasi=' .@$id_jadwal_konsultasi) ?>";
+          location.href = "<?php echo base_url('pasien/ResepDokter/konfirmasi/'.$id_jadwal_konsultasi) ?>";
         }
         if (JSON.parse(JSON.parse(payload.data.body).name == 'vp') || JSON.parse(JSON.parse(payload.data.body).name == 'universal')) {
           $("#isinotifmodal").text(JSON.parse(payload.data.body).keterangan);

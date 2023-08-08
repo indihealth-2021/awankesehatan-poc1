787da78da2736a50ef84d6c78f14fdae0fad2b04
f3iru <this.fairuz@gmail.com>
2023-08-08 11:53:18 +0700

    prep config

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,9 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'https://poc1.telemedical.id';//'https://telemedicinelintasdev.indihealth.com';//'http://125.213.128.90/telemedicinelintas2';//'https://telemedicinelintas.indihealth.com/';//'http://125.213.128.90/konsultasi-dokter/';
-// $config['path_to_env'] = 'https://poc1.telemedical.id/';
-// $config['path_to_api'] = 'https://api-poc1.telemedical.id/';
+// $config['base_url'] = 'https://poc1.awankesehatan.com';//'https://telemedicinelintasdev.indihealth.com';//'http://125.213.128.90/telemedicinelintas2';//'https://telemedicinelintas.indihealth.com/';//'http://125.213.128.90/konsultasi-dokter/';
+// $config['path_to_env'] = 'https://poc1.awankesehatan.com/';
+// $config['path_to_api'] = 'https://api-poc1.awankesehatan.com/';
 
 $config["base_url"] = "http://localhost/poc1";
 $config["path_to_env"] = "http://localhost/poc1";
